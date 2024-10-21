198d54dd67e89884c0ab7303ec9f80a80173a836
Meriem Al Ahmadi Hammou <Meriem.Al.Ahmadi.Hammou@msg.group>
2024-10-21 10:21:38 +0200

    First angular app

---
+++ src/app/header.component.ts
@@ -0,0 +1,9 @@
+import {Component}  from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  templateUrl: './header.component.html'
+})
+export class HeaderComponent{}
