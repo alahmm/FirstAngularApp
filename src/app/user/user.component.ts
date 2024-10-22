4920095be2884879e91a0d8354d786471c01830f
Meriem Al Ahmadi Hammou <Meriem.Al.Ahmadi.Hammou@msg.group>
2024-10-22 10:16:06 +0200

    Use Output decorative for events

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import {Component, computed, EventEmitter, Input, input, Output} from '@angular/core';//Input is a decorator and input is a special function for signals
 
 @Component({
   selector: 'app-user',
@@ -7,12 +7,15 @@ import {Component, Input} from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
+  @Input({required: true}) id!: string;
   @Input({required: true}) avatar!: string;  //mark this property as settable from outside, ! for seeing to typescript that this field gonna be initialized from outside
   @Input({required: true}) name!: string;
-
+  @Output() select = new EventEmitter();
   get imagePath() {
     return 'assets/users/' + this.avatar;
   }
-  onSelectUser() {}
+  onSelectUser() {
+    this.select.emit(this.id);
+  }
 
 }
