f225423cdddf6b76bacd761b52011839861403f0
Meriem Al Ahmadi Hammou <Meriem.Al.Ahmadi.Hammou@msg.group>
2024-10-22 12:23:31 +0200

    Create a separate type script type

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,10 @@
 import {Component, computed, EventEmitter, Input, input, Output, output} from '@angular/core';//Input is a decorator and input is a special function for signals
 
+type User = {
+  id: string,
+  avatar: string,
+  name: string
+};
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -7,11 +12,7 @@ import {Component, computed, EventEmitter, Input, input, Output, output} from '@
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  @Input({required: true}) user!: {
-    id: string,
-    avatar: string,
-    name: string
-  };//creating a type for user
+  @Input({required: true}) user!: User;//creating a type for user
   @Output() select = new EventEmitter();//you can explicitly specify the type through <string>
   get imagePath() {
     return 'assets/users/' + this.user.avatar;
