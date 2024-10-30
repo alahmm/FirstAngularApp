14bac1b03ab63b42a5db51ef432b4222147b1126
Meriem Al Ahmadi Hammou <Meriem.Al.Ahmadi.Hammou@msg.group>
2024-10-30 13:18:40 +0100

    Use localStorage for data persistence

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -31,6 +31,13 @@ export class TasksService {
   ];
   nextId = this.tasks.length + 1;
 
+  constructor() {
+    const tasks = localStorage.getItem('tasks');
+    if(tasks) {
+      this.tasks = JSON.parse(tasks);
+    }
+  }
+
   getUserTasks(userId: string) {
     return this.tasks.filter(task => task.userId === userId);
   }
@@ -45,9 +52,14 @@ export class TasksService {
         dueDate: taskData.dueDate
       }
     )
+    this.saveTasks();
   }
 
   removeTask(id: string) {
     this.tasks = this.tasks.filter(task => task.id !== id);
+    this.saveTasks();
+  }
+  private saveTasks() {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
   }
 }
